b984960f7ceefaf3bbc31ba70e7046bcfbe3e6bd
mkichenin-svg <mkichenin@mden-reunion.fr>
2026-05-20 08:37:20 +0400

    anru

--- app.R
+++ app.R
@@ -1,5 +1,4 @@
 
-
 library(shiny)
 library(bslib)
 library(ggplot2)
@@ -11,56 +10,102 @@ shapefile <-read_sf("QPV/quartiers-prioritaires-de-la-politique-de-la-ville-qpv.
 shapefile1 <-read_sf("communes/communesPolygon.shp")
 iris <- read_sf("iris/georef-france-iris-millesime.shp")
 
+genre <- read.csv2("genre.csv")
+genre
 
-ui <- fluidPage( 
+ui <-page_fluid( 
   
       titlePanel(
         "NPNRU Rive-Droite - Saint-Benoît"
       ),
   
-  theme = theme_bootswatch("superhero"),
+  theme = theme_bootswatch("minty"),
+  
 
-
+  
   # Première page
+  layout_column_wrap(
+    
+    value_box( 
+      title = "", 
+      "34 025 heures à réaliser", 
+      "19 726 heures financées ANRU", 
+      showcase = bsicons::bs_icon("clock"),
+      showcase_layout = "left center",
+      theme = "primary",
+      height = 100
+    ) ,
+    
+  value_box( 
+    title = "", 
+    "12 283 heures réalisées", 
+    "62% des objectifs conventionnés ANRU", 
+    showcase = bsicons::bs_icon("calendar2-check"),
+    showcase_layout = "left center",
+    theme = "primary",
+    height = 100
+  ) ,
+  
+  value_box( 
+    title = "", 
+    "38 bénéficiaires", 
+    theme = "primary", 
+    class = "border",
+    showcase = bsicons::bs_icon("person-fill-check"),
+    height = 100
+  ) ,
+  
+  value_box( 
+    title = "", 
+    "34 bénéficiaires issus d'un QPV", 
+    "89% du total", 
+    theme = "primary",
+    showcase = bsicons::bs_icon("buildings"),
+    height = 100
+  ) ,
+  
+  ),
     
     layout_column_wrap(
       
+      
       card(
-        plotOutput("map", width = 500, height = 500),
+        plotOutput("map", width = 700, height = 700),
         
       ),
       
+     
       card(
-        card_header("LES DONNÉES À REMONTER anru ddzs    "),
-        card_body(height = 30),
-        p("- nombre d’heures travaillées pour les opérations liées aux travaux et dans le cadre de la gestion urbaine de proximité"),
-        p("- modalités de réalisation des heures (embauche directe, intérim, alternance, formation…)"),
-        p("- typologie des entreprises attributaires (nombre de salariés, secteur d’activité…)"),
-        p("- typologie des bénéficiaires : sexe, âge, résidence dans un quartier prioritaire de la politique de la ville, …"),
-        p(" - situation des bénéficiaires à 6 et 12 mois après leur entrée dans le dispositif ;"),
-        p(" - embauches directes ou indirectes liées à l’ingénierie des projets, au fonctionnement des équipements et aux actions d’accompagnement")
-  
-      )
-      
-      
-    ),
-    
-    
-    layout_column_wrap(
-      
-      card(
-        card_header(),
-        tableOutput("echelle")
+        value_box( 
+          title = "", 
+          "75 % des bénéficaires ont moins de 40 ans",
+          class = "border",
+          height = 10
+        ) ,
+        
+        
+        plotOutput("age", height = 70),
+        
+        value_box( 
+          title = "", 
+          "Bénéficiaires majoritairement masculins (32/34)",
+          class = "border",
+          height = 10
+        ) ,
+        plotOutput("genre", height = 70),
+        
       ),
       
-      card(
-        card_header(),
-        tableOutput("convent")
-   ),
-),
-    
-    
+     
+    ),
   
+
+layout_column_wrap(  
+  
+imageOutput("anru", height = 500),
+imageOutput("image", height = 400, width = 400),  
+imageOutput("mden", height = 200, width = 100),
+) 
  
 )
   
@@ -73,13 +118,39 @@ server <- function(input, output) {
   
   
   
-  
   output$image <- renderImage( 
     { 
-      list(src = "logo.png", height = "30%") 
-    }, 
-    deleteFile = FALSE 
+      list(src = "logo.jpg", height = "30%") 
+    }
   ) 
+  
+  output$anru <- renderImage( 
+    { 
+      list(src = "anru.png", height = "30%") 
+    }
+  ) 
+  
+  output$mden <- renderImage( 
+    { 
+      list(src = "mden.png", height = "25%") 
+    }
+  ) 
+  
+  output$genre <- renderPlot(
+    
+    { 
+      ggplot(genre,aes(x= genre, y = nombre, fill = genre)) + geom_bar(stat = "identity", show.legend = FALSE) + theme_minimal() +
+        theme(axis.title = element_blank(), axis.text.y = element_blank()) 
+      }
+  )
+  
+  output$age <- renderPlot(
+    
+    { 
+      ggplot(age,aes(x= âge, y = Nombre, fill = âge)) + geom_bar(stat = "identity", show.legend = FALSE) + theme_minimal()+
+        theme(axis.title = element_blank(), axis.text.y  = element_blank()) 
+    }
+  )
   
   # carte
   
@@ -91,8 +162,7 @@ server <- function(input, output) {
       geom_sf(data = iris[9,31])+
       geom_sf(data = iris[12,31])+
       geom_sf(data = iris[21,31])+
-      geom_sf(data= shapefile[36,], color = "black", fill="orange", alpha= 7/10
-              ) +
+      geom_sf(data= shapefile[36,], color = "black", fill="orange", alpha= 7/10) +
       geom_sf_label(data= iris[3,31], label = "Beaufond_Le_Port")+
       geom_sf_label(data = iris[9,31], label = "Beaufond_Distillerie")+
       geom_sf_label(data = iris[12,31], label = "Centre ville-Rive gauche")+
@@ -103,26 +173,11 @@ server <- function(input, output) {
   }) 
   
   
-  output$echelle <- renderTable({ echelle
-    
-    
-  }) 
- 
-  output$convent <- renderTable({ convent
-   
-    
-  }) 
-  
-  
-  
-  
 
-  
-  
+ 
 
   
 }
-
 
 
 # Run the app ----
