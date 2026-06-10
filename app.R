55b1018d2243bdd3f8bde0a319e1e3b94b4b18d3
mkichenin-svg <mkichenin@mden-reunion.fr>
2026-06-10 10:57:02 +0400

    anru

--- app.R
+++ app.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(sf)
 library(tibble)
 library(bsicons)
-library(gt)
+library(reactable)
 
 
 shapefile <-read_sf("QPV/quartiers-prioritaires-de-la-politique-de-la-ville-qpv.shp")
@@ -131,7 +131,7 @@ ui <-page_navbar( title = div("POINT D'ÉTAPE ANRU ", class="custom title"),
       card(
         
         nav_panel(width=200,"Avancement des heures d'insertion",
-                gt_output("heures_saint_denis")),
+                reactableOutput("heures_saint_denis")),
       
       ),
      
@@ -646,8 +646,8 @@ server <- function(input, output) {
 \n19 comités d’insertion, à raison d’un par trimestre, permettent un suivi régulier de la mise en œuvre du volet d’insertion du projet PRUNEL. 
 \nSuperficie = 383 593 m²  "})
   
-  output$heures_saint_denis <- render_gt({gt(heures_saint_denis)})
-  output$echelle_saint_denis <- renderTable({head(echelle_saint_denis)})
+  output$heures_saint_denis <- renderReactable({reactable(heures_saint_denis)})
+  output$echelle_saint_denis <- renderReactable({reactable(echelle_saint_denis)})
   output$MO_saint_denis <- renderTable({head(MO_saint_denis)})
   
   output$map <- renderPlot({ 
